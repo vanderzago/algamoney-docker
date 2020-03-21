71e2eaa12419bcb4c74fc1d3514379e1c70515c7
Vander Alves <vanderzago@gmail.com>
2020-03-21 18:19:25 -0300

    condicao de parada do set.sh

--- up.sh
+++ up.sh
@@ -2,6 +2,18 @@
 
 echo "=> Start all"
 echo
-./up_infra.sh
-./set_infra.sh
+./up_infra.sh; Res=$?
+
+if [[ $Res -ne 0 ]]
+then
+    exit 1
+fi
+
+./set_infra.sh; Res=$?
+
+if [[ $Res -ne 0 ]]
+then
+    exit 1
+fi
+
 ./up_app.sh
